9638c8c623725b6a80ccdfc60a9fb11c0bf3713b
Arky Asmal <arkyasmal@gmail.com>
2023-03-01 13:34:15 -0500

    fixed bug where private key is not found due to whitespace being removed first

--- utils/google/initalizeGoogleDrive.ts
+++ utils/google/initalizeGoogleDrive.ts
@@ -1,16 +1,17 @@
 import { setUpOathClient } from "./setupOathClient";
 import { google } from "googleapis";
 import { CredentialBody } from "google-auth-library";
-
+// import * as dotenv from 'dotenv'
+//   dotenv.config();
 export const extractCredentialsObj = (creds?: string) => {
   if (!creds) return {};
   const startOfKey = "-----BEGIN PRIVATE KEY-----";
   const endOfKey = "-----END PRIVATE KEY-----";
   const matchRegex = new RegExp(`${startOfKey}(.*)${endOfKey}`, "gsm");
-  const credsNoSpace = creds.replace(/\\"/g, `"`).replace(" ", "");
+  const credsNoEscape = creds.replace(/\\"/g, `"`);
   const privateKey =
-    credsNoSpace.match(matchRegex)?.[0].replace(/\\\n/g, "\n").replace(/\\n/g, '\n') + "\n";
-  const credsNoLines = credsNoSpace.replace(/\\\n/g, "").replace(/\n/g, "");
+    credsNoEscape.match(matchRegex)?.[0].replace(/\\\n/g, "\n").replace(/\\n/g, '\n') + "\n";
+  const credsNoLines = credsNoEscape.replace(/ /g, "").replace(/\\\n/g, "").replace(/\n/g, "");
   const subCred = credsNoLines.substring(1, credsNoLines.length - 1);
   const result = JSON.parse(subCred);
   result["private_key"] = privateKey;
