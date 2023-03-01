732c89771dc71205686d9c4f6bfd76d0f97df37d
Arky Asmal <arkyasmal@gmail.com>
2023-03-01 01:15:46 -0500

    added return value

--- app/lib/webhooks/resources/watch/googleDriveChannel/googleDriveChannelPut/index.ts
+++ app/lib/webhooks/resources/watch/googleDriveChannel/googleDriveChannelPut/index.ts
@@ -36,67 +36,70 @@ export async function handler(
   const parsed = parseCredentialsVariable(
     process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS
   );
-  return parsed
-  const drive = initalizeGoogleDrive(
-    process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS
-  );
-  // console.log(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS);
-  const domainName = convertToStr(process.env.WEBHOOKS_API_DOMAIN_NAME);
-  const currDate = new Date();
-  const endWatchDate = add(currDate, {
-    hours: 13,
-  });
-  //create a channel watch
-  const folderName = convertToStr(process.env.GOOGLE_DRIVE_FOLDER_NAME);
-  const result = await drive.files.list({
-    q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}' and trashed=false`,
-    fields: "files(id,parents,name)",
-  });
-  const folders = result.data.files;
-  if (!folders)
-    return {
-      statusCode: 500,
-      body: `Folder called ${folderName} cannot be found`,
-    };
-  const parentIds = folders.map((e) => (e.parents ? e.parents[0] : null));
-  const correctIdx = await identifyCorrectFolder(
-    drive,
-    parentIds,
-    convertToStr(process.env.GOOGLE_DRIVE_PARENT_FOLDER_NAME)
-  );
-  if (correctIdx === undefined)
-    return {
-      statusCode: 500,
-      body: `Folder called ${folderName} cannot be identified correctly`,
-    };
-  const folderId = folders[parseInt(correctIdx)].id;
-  try {
-    //get page token
-    const {
-      data: { startPageToken },
-    } = await drive.changes.getStartPageToken();
-    //get watch changes
-    const watchRes = await drive.changes.watch({
-      pageToken: convertToStr(startPageToken),
-      requestBody: {
-        resourceId: folderId,
-        id: uuid(),
-        kind: "api#channel",
-        token: process.env.WEBHOOKS_API_TOKEN,
-        type: "web_hook",
-        expiration: (getUnixTime(endWatchDate) * 1000).toString(),
-        address: `https://${domainName}/googleDrive`,
-      },
-    });
-    return {
-      statusCode: 200,
-      body: JSON.stringify(watchRes),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(err),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify(parsed)
   }
+  // const drive = initalizeGoogleDrive(
+  //   process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS
+  // );
+  // // console.log(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS);
+  // const domainName = convertToStr(process.env.WEBHOOKS_API_DOMAIN_NAME);
+  // const currDate = new Date();
+  // const endWatchDate = add(currDate, {
+  //   hours: 13,
+  // });
+  // //create a channel watch
+  // const folderName = convertToStr(process.env.GOOGLE_DRIVE_FOLDER_NAME);
+  // const result = await drive.files.list({
+  //   q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}' and trashed=false`,
+  //   fields: "files(id,parents,name)",
+  // });
+  // const folders = result.data.files;
+  // if (!folders)
+  //   return {
+  //     statusCode: 500,
+  //     body: `Folder called ${folderName} cannot be found`,
+  //   };
+  // const parentIds = folders.map((e) => (e.parents ? e.parents[0] : null));
+  // const correctIdx = await identifyCorrectFolder(
+  //   drive,
+  //   parentIds,
+  //   convertToStr(process.env.GOOGLE_DRIVE_PARENT_FOLDER_NAME)
+  // );
+  // if (correctIdx === undefined)
+  //   return {
+  //     statusCode: 500,
+  //     body: `Folder called ${folderName} cannot be identified correctly`,
+  //   };
+  // const folderId = folders[parseInt(correctIdx)].id;
+  // try {
+  //   //get page token
+  //   const {
+  //     data: { startPageToken },
+  //   } = await drive.changes.getStartPageToken();
+  //   //get watch changes
+  //   const watchRes = await drive.changes.watch({
+  //     pageToken: convertToStr(startPageToken),
+  //     requestBody: {
+  //       resourceId: folderId,
+  //       id: uuid(),
+  //       kind: "api#channel",
+  //       token: process.env.WEBHOOKS_API_TOKEN,
+  //       type: "web_hook",
+  //       expiration: (getUnixTime(endWatchDate) * 1000).toString(),
+  //       address: `https://${domainName}/googleDrive`,
+  //     },
+  //   });
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(watchRes),
+  //   };
+  // } catch (err) {
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify(err),
+  //   };
+  // }
 }
 
