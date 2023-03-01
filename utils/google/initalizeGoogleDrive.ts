59f38157c980877de10fb16a3cb1e32a998e8b00
Arky Asmal <arkyasmal@gmail.com>
2023-03-01 12:53:43 -0500

    add cred file

--- utils/google/initalizeGoogleDrive.ts
+++ utils/google/initalizeGoogleDrive.ts
@@ -1,22 +1,33 @@
 import { setUpOathClient } from "./setupOathClient";
 import { google } from "googleapis";
-import { CredentialBody, ExternalAccountClientOptions } from 'google-auth-library';
+import { CredentialBody } from "google-auth-library";
+
+export const extractCredentialsObj = (creds?: string) => {
+  if (!creds) return {};
+  const startOfKey = "-----BEGIN PRIVATE KEY-----";
+  const endOfKey = "-----END PRIVATE KEY-----";
+  const matchRegex = new RegExp(`${startOfKey}(.*)${endOfKey}`, "gsm");
+  const credsNoSpace = creds.replace(/\\"/g, `"`).replace(" ", "");
+  const privateKey =
+    credsNoSpace.match(matchRegex)?.[0].replace(/\\\n/g, "\n") + "\n";
+  const credsNoLines = credsNoSpace.replace(/\\\n/g, "").replace(/\n/g, "");
+  const subCred = credsNoLines.substring(1, credsNoLines.length - 1);
+  const result = JSON.parse(subCred);
+  result["private_key"] = privateKey;
+  return result;
+};
 export const parseCredentialsVariable = (credentials?: string) => {
   let parsed: any;
   try {
     parsed = JSON.parse(typeof credentials === "string" ? credentials : "");
   } catch (e) {
-    const parseDopplerSecret =
-      typeof credentials === "string" ? credentials.replace(/\\"/g, `"`) : `""`;
-    const removeEnds = parseDopplerSecret.substring(
-      1,
-      parseDopplerSecret.length - 1
-    );
-    parsed = typeof credentials === "string" ? JSON.parse(removeEnds) : {};
+    parsed = extractCredentialsObj(credentials);
   }
   return parsed;
 };
-export const initalizeGoogleDrive = (credentials: CredentialBody | undefined) => {
+export const initalizeGoogleDrive = (
+  credentials: CredentialBody | undefined
+) => {
   const authClient = setUpOathClient({
     credentials: credentials,
   });
@@ -26,4 +37,3 @@ export const initalizeGoogleDrive = (credentials: CredentialBody | undefined) =>
   });
   return drive;
 };
-
