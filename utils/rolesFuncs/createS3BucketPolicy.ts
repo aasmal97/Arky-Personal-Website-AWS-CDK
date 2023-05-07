c7b5e7357ad931eaa2de9d0ebd26ad4e1f94efa7
Arky Asmal <arkyasmal@gmail.com>
2023-05-06 21:01:35 -0400

    added wildcard permissions to delete, put and get actions in s3 buckets for generating s3 bucket permissions

--- utils/rolesFuncs/createS3BucketPolicy.ts
+++ utils/rolesFuncs/createS3BucketPolicy.ts
@@ -9,17 +9,20 @@ export const createS3BucketPolicy = (
 ): aws_iam.PolicyDocument => {
   const getActions = [
     "s3:GetObject",
+    "s3:GetObject*",
     "s3:GetObjectAttributes",
     "s3:GetObjectTagging",
     "s3:GetObjectVersion",
   ];
   const putActions = [
     "s3:PutObject",
+    "s3:PutObject*",
     "s3:PutObjectTagging",
     "s3:PutObjectVersionTagging",
   ];
   const deleteActions = [
     "s3:DeleteObject",
+        "s3:DeleteObject*",
     "s3:DeleteObjectTagging",
     "s3:DeleteObjectVersionTagging",
     "s3:DeleteObjectVersion",
