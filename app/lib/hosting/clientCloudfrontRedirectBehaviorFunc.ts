94d58f337e81b361d6d2c2285ae3571ab9ed4628
Arky Asmal <arkyasmal@gmail.com>
2025-01-03 19:24:41 -0500

    update redirect

--- app/lib/hosting/clientCloudfrontRedirectBehaviorFunc.ts
+++ app/lib/hosting/clientCloudfrontRedirectBehaviorFunc.ts
@@ -1,25 +1,26 @@
 function handler(event: any) {
-  var request = event.request;
-  var uri = request.uri;
-  if(uri.endsWith("/")) {
-    request.uri += "index.html";
-    return request
-  }
-  //find origin
-  var regex = /https?:\/\/.+((.org)|(.com))/;
-  var uri_path_arr = uri.split(regex);
-  var uri_path = uri_path_arr[uri_path_arr.length - 1];
-  var sections = uri_path.split("/");
-  var lastSection = sections[sections.length - 1];
-  if (!lastSection.includes(".")) {
-    lastSection += "/index.html";
-    lastSection = lastSection.replace("//", "/");
-    var host = request.uri.match(regex, "/g")[0];
-    if (!lastSection.startsWith("/")) {
-      lastSection = "/" + lastSection;
+  const updateURL = () => {
+    var request = event.request;
+    var uri = request.uri;
+    if (uri.endsWith("/")) {
+      request.uri += "index.html";
+      return request;
     }
-    request.uri = host + lastSection;
+    //find origin
+    var regex = /https?:\/\/.+((.org)|(.com))/;
+    var uri_path_arr = uri.split(regex);
+    var uri_path = uri_path_arr[uri_path_arr.length - 1];
+    var sections = uri_path.split("/");
+    var lastSection = sections[sections.length - 1];
+    if (!lastSection.includes(".")) {
+      request.uri += "/index.html";
+    }
+    return request;
+  };
+  try {
+    updateURL();
+  } catch (err) {
+    return event.request;
   }
-  return request;
 }
 // export default handler
